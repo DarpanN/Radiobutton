cb7ba1096c2fd9f6ad2fb3cd16e249a543c8b64d
DarpanN <cloversdnc1994@gmail.com>
2019-10-21 11:40:05 +0545

    Initial commit

---
+++ app/src/main/java/com/app/myapplicationradiobutton/activity_life_cycle.java
@@ -0,0 +1,14 @@
+package com.app.myapplicationradiobutton;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class activity_life_cycle extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_life_cycle);
+    }
+}
